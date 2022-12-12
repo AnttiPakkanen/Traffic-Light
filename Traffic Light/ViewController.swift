6f958334eb65f1e9e9a0c932e565ce704b57b765
Andrei Pakkanen <a.pakkanen@me.com>
2022-12-12 17:48:23 +0600

    End of the project

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -9,11 +9,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet var trafficLightButton: UIButton!
+    @IBOutlet var greenTrafficLight: UIView!
+    @IBOutlet var yellowTrafficLight: UIView!
+    @IBOutlet var redTrafficLight: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redTrafficLight.layer.cornerRadius = redTrafficLight.frame.width / 2
+        yellowTrafficLight.layer.cornerRadius = yellowTrafficLight.frame.width / 2
+        greenTrafficLight.layer.cornerRadius = greenTrafficLight.frame.width / 2
+        trafficLightButton.layer.cornerRadius = 10
     }
-
-
+    
+    enum TrafficLight {
+        case redLight
+        case yellowLight
+        case greenLight
+        case trafficLightOff
+    }
+    
+    var trafficLight = TrafficLight.redLight
+    
+    @IBAction func trafficLightButtonDidTapped() {
+        trafficLightButton.setTitle("NEXT", for: .normal)
+        
+        switch trafficLight {
+            case .redLight:
+                redTrafficLight.alpha = 1
+                yellowTrafficLight.alpha = 0.3
+                greenTrafficLight.alpha = 0.3
+                trafficLight = .yellowLight
+            case .yellowLight:
+                redTrafficLight.alpha = 0.3
+                yellowTrafficLight.alpha = 1
+                greenTrafficLight.alpha = 0.3
+                trafficLight = .greenLight
+            case .greenLight:
+                redTrafficLight.alpha = 0.3
+                yellowTrafficLight.alpha = 0.3
+                greenTrafficLight.alpha = 1
+                trafficLight = .trafficLightOff
+            case . trafficLightOff:
+                redTrafficLight.alpha = 0.3
+                yellowTrafficLight.alpha = 0.3
+                greenTrafficLight.alpha = 0.3
+                trafficLight = .redLight
+                trafficLightButton.setTitle("START", for: .normal)
+            }
+       
+        
+    }
+    
+    
+    
 }
 
